14081a7218684ced4f7ef18d15f3aa995dce1d49
sakib-atreus <sakibatreus@gmail.com>
2025-01-03 23:57:33 +0600

    update

--- src/app/modules/reviews/review.validation.ts
+++ src/app/modules/reviews/review.validation.ts
@@ -4,15 +4,15 @@ import { z } from "zod";
 const reviewValidationSchema = z.object({
     productId: z
     .string()
-    .nonempty("Product ID is required"),
+    .nonempty("Product ID is required!"),
     userId: z
     .string()
-    .nonempty("User ID is required"),
+    .nonempty("User ID is required!"),
     rating: z
         .number()
         .min(1, "Rating must be at least 1")
         .max(5, "Rating must not exceed 5"),
-    comment: z.string().nonempty("Comment is required"),
+    comment: z.string().nonempty("Comment is required!"),
     createdAt: z.date().default(() => new Date()), // Defaults to the current date if not provided
 });
 
