538049641e256532aac6400781923f07f13ae800
sakib-atreus <sakibatreus@gmail.com>
2025-01-04 22:09:12 +0600

    user validation

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -6,11 +6,11 @@ const roleEnum = z.enum(nameEnum as [string, ...string[]])
 
 const userCreateValidationSchema = z.object({
   body: z.object({
-    name: z.string().min(1, { message: 'Name is required' }),
-    email: z.string().email({ message: 'Invalid email address' }),
+    name: z.string().min(1, { message: 'Name is required!' }),
+    email: z.string().email({ message: 'Invalid email address!' }),
     password: z
       .string()
-      .min(8, { message: 'Password must be at least 8 characters long' }),
+      .min(6, { message: 'Password must be at least 6 characters long' }),
     phone: z.string().min(1, { message: 'Phone number is required' }),
     role: roleEnum,
     address: z.string().min(1, { message: 'Address is required' }),
