934451fb2a468c3450c0771b63e2a8766ee049ae
sakib-atreus <sakibatreus@gmail.com>
2025-02-20 21:27:43 +0600

    api

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/', routes)
+app.use('/api/v1', routes)
 
 // application routes
 // app.use('/api/products', ProductRoute);
