a0f08dff6f23f31d38200231757816c4b4b86c71
sakib-atreus <sakibatreus@gmail.com>
2025-03-02 00:00:20 +0600

    update

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -13,7 +13,7 @@ const userCreateValidationSchema = z.object({
       .min(6, { message: 'Password must be at least 6 characters long!' }),
     phone: z.string().min(1, { message: 'Phone number is required!' }),
     role: roleEnum,
-    address: z.string().min(1, { message: 'Address is required!!' }),
+    address: z.string().min(1, { message: 'Address is required!' }),
   }),
 })
 
