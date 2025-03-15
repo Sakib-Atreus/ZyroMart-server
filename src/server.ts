d3fcb8ee17f1c6416b9503ee4b534b431b81c383
sakib-atreus <sakibatreus@gmail.com>
2025-03-16 03:00:07 +0600

    seeder update

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import mongoose from 'mongoose';
 import config from './app/config';
 import app from './app';
+import adminSeeder from './app/modules/users/seeders/adminSeeder';
 
 async function main() {
   try {
     console.log("connecting to mongodb....⏳");
     await mongoose.connect(config.db_url as string);
     console.log("Database connected successfully... ✅");
+
+    // admin seeder
+    await adminSeeder();
 
     app.listen(config.port, () => {
       console.log(`ZyroMart E-commerce management server app listening on port : ${config.port}`);
