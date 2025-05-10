e38c4745a57d13d02c19d929b3c6e7282f4b2bb8
sakib-atreus <sakibatreus@gmail.com>
2025-05-10 23:45:35 +0600

    route secure update

--- src/app/modules/products/product.route.ts
+++ src/app/modules/products/product.route.ts
@@ -41,10 +41,19 @@ import { filterMiddleware } from '../../middleware/filterProductMiddleware';
 
 const router = express.Router();
 
+// route for creating a new product
 router.post('/', auth(USER_ROLE.admin, USER_ROLE.user), ProductControllers.createProduct);
+
+// route for getting all products with filter middleware
 router.get('/', filterMiddleware, ProductControllers.getAllProducts);
+
+// route for getting a single product
 router.get('/:productId', ProductControllers.getSingleProduct);
+
+// route for deleting a single product
 router.delete('/:productId', auth(USER_ROLE.admin, USER_ROLE.user), ProductControllers.deleteProduct);
+
+// route for updating a single product
 router.put('/:productId', auth(USER_ROLE.admin, USER_ROLE.user), ProductControllers.updateProduct);
 
 export const ProductRoute = router;
